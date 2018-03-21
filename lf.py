0f992777ace21fdcc8a9d992f948970098b4d19f
SamC <sc14809@my.bristol.ac.uk>
2018-03-21 16:03:05 +0000

    hopefully managed to grab all spike information from each tetrode

--- lf.py
+++ lf.py
@@ -3,7 +3,14 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import decoder as bd
 
-pos_info = sio.loadmat('Con/conpos01.mat')
-epoch = 0
-pos_epoch_info = pos_info['pos'][0][0][0][epoch][0][0]
-print(pos_epoch_info['data'])
+day = 0   # int in [0,5]
+epoch = 0 # int in [0,4]
+
+# pos info
+pos_mat = sio.loadmat('Con/conpos%02d.mat' % (day+1))
+pos_info = pos_mat['pos'][0][day][0][epoch][0][0]
+
+# spike info
+spk_mat = sio.loadmat('Con/conspikes%02d.mat' % (day+1))
+spk_info = spk_mat['spikes'][0][day][0][epoch][0]
+spikes = np.array([units[0]['data'][0] for tetrode in spk_info for units in tetrode[0] if len(units) > 0])
