f82047f02b19d754abe71a37b69c92e5fa5ebf90
SamC <sc14809@my.bristol.ac.uk>
2018-03-21 20:08:34 +0000

    more processing

--- lf.py
+++ lf.py
@@ -8,9 +8,13 @@ epoch = 0 # int in [0,4]
 
 # pos info
 pos_mat = sio.loadmat('Con/conpos%02d.mat' % (day+1))
-pos_info = pos_mat['pos'][0][day][0][epoch][0][0]
+pos_info = pos_mat['pos'][0][day][0][epoch][0]['data'][0]
+pos = pos_info[:,0:3]
+
+# epoch info
+epoch_info = np.array([min(pos_info[:,0]),max(pos_info[:,0])])
 
 # spike info
 spk_mat = sio.loadmat('Con/conspikes%02d.mat' % (day+1))
-spk_info = spk_mat['spikes'][0][day][0][epoch][0]
-spikes = np.array([units[0]['data'][0] for tetrode in spk_info for units in tetrode[0] if len(units) > 0])
+spk_info = np.array([units[0]['data'][0] for tetrode in spk_mat['spikes'][0][day][0][epoch][0] for units in tetrode[0] if len(units) > 0])
+spk = np.array([unit[:,0] for unit in spk_info])
